cff5deb833c1106843af96b5279ebf7688b5e422
Mediatro <mediatro.dev@gmail.com>
2021-11-12 10:20:15 +0300

    payment detail list to table

--- src/modules/eocs/payments/components/PaymentDetailsList.js
+++ src/modules/eocs/payments/components/PaymentDetailsList.js
@@ -1,4 +1,15 @@
-import {Box, IconButton, List, ListItem, ListItemText, Paper, Typography} from "@mui/material";
+import {
+    Box,
+    IconButton,
+    List,
+    ListItem,
+    ListItemText,
+    Paper,
+    Table, TableBody, TableCell, TableContainer,
+    TableHead,
+    TableRow,
+    Typography
+} from "@mui/material";
 import {FormattedMessage} from "react-intl";
 import {useContext} from "react";
 import {AuthContext} from "../../../shared/services/AuthProvider";
@@ -7,6 +18,7 @@ import LoopIcon from '@mui/icons-material/Loop';
 import {PaymentManagerContext} from "../services/PaymentProvider";
 import {TCardTitle} from "../../../shared/components/TCardTitle";
 import {TStatus} from "../../../shared/components/TStatus";
+import RequestQuoteIcon from "@mui/icons-material/RequestQuote";
 
 export function PaymentDetailsList(){
 
@@ -45,7 +57,46 @@ export function PaymentDetailsList(){
                 <FormattedMessage id={'payment.text.payment_detail.list'}/>
             </TCardTitle>
 
-            <List>
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell><FormattedMessage id={'payment.field.payment_detail.display_string'}/></TableCell>
+                            <TableCell align="right"><FormattedMessage id={'payment.field.payment_detail.status'}/></TableCell>
+                            <TableCell align="right"></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {authc.manager.getUser().paymentDetails.slice(0).reverse().map((detail, i) => (
+                            <TableRow>
+                                <TableCell component="th"
+                                           scope="row">{detail.displayString}</TableCell>
+                                <TableCell align="right">
+                                    <TStatus status={detail.status}>
+                                        <FormattedMessage id={`payment.field.payment_detail.status.${detail.status}`}/>
+                                    </TStatus>
+                                </TableCell>
+                                <TableCell align="right">
+                                    {authc.manager.getUser().activePaymentDetail && authc.manager.getUser().activePaymentDetail['@id'] === detail['@id']
+                                        ?
+                                            <Typography variant={'h6'}>
+                                                <FormattedMessage id={'payment.field.payment_detail.active'}/>
+                                            </Typography>
+                                        :
+                                            detail.status === 'verified' &&
+                                                <IconButton color={'primary'} onClick={() => activateDetail(detail)}>
+                                                    <LoopIcon/>
+                                                </IconButton>
+                                    }
+
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+            {/*<List>
                 {authc.manager.getUser().paymentDetails.slice(0).reverse().map((detail, i) => (
                     <ListItem secondaryAction={getListAction(detail)}>
                         <ListItemText>
@@ -53,7 +104,7 @@ export function PaymentDetailsList(){
                         </ListItemText>
                     </ListItem>
                 ))}
-            </List>
+            </List>*/}
         </SPaper>
     );
 }
